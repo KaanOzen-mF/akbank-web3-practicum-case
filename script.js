c18a2c623a3df331df6262730afc465e4eac037c
KaanOzen-mF <kaanozn98@gmail.com>
2022-09-10 16:25:12 +0300

    Case is done

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 function oddishOrEvenish(n) {
-	let sum;
-	if (n < 0) {
-		alert("Please write a positive number");
-	} else {
-		for (sum = 0; n > 0; sum += n % 10, n = parseInt(n / 10));
+	var sum = 0;
 
-		return sum % 2 == 0 ? "even" : "odd"; //ternary operator use for checking our sum values odd or even
+	while (n != 0) {
+		sum = sum + (n % 10);
+		n = parseInt(n / 10);
 	}
+	return sum % 2 == 0 ? "Even" : "Odd";
 }
 
 function returnNumber() {
