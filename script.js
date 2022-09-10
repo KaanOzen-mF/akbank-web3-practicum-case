35f4217184e6339f2451adaee7b3ad8ac0d1b445
KaanOzen-mF <kaanozn98@gmail.com>
2022-09-10 10:08:40 +0300

    Odd or even function is done

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-function oddishOrEvenish() {}
+function oddishOrEvenish(n) {
+	let sum;
+
+	for (sum = 0; n > 0; sum += n % 10, n = parseInt(n / 10));
+
+	return console.log(sum % 2 == 0 ? "even" : "odd"); //ternary operator use for checking our sum values odd or even
+}
+
+let n = 43;
+
+oddishOrEvenish(n);
